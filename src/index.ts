d0053f1c9dc059444398d45e738fdc1655c4d30f
Zalán Horváth <pityuespetra@gmail.com>
2021-02-11 17:17:39 +0100

    asd

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ const bot: any = new Discord.Client();
 import DBL = require("dblapi.js");
 let ms = require("ms");
 import * as admin from 'firebase-admin';
-import https = require('https');
 import express = require('express');
 
 const app = express();
@@ -210,36 +209,12 @@ bot.on("message", async (message: any) => {
 
     //count 
     else if(message.content === `${prefix}count`){
-        await getCountForUser(message);
+        await bot.commands.get('count').execute(message);
     }
     else if(cmd === `${prefix}link`) {
-        if(!args[0]){
-            message.reply("Please provide your email");
-        } else if(!args[1]) {
-            message.reply("Please provide your link code");
-        } else {
-            botTypeing(message.channel.id);
-            admin
-                .auth()
-                .getUserByEmail(args[0])
-                .then((userRecord: any) => {
-                    return accountLink(userRecord, message);
-                })
-                .catch((error: any) => {
-                    console.log("Error fetching user data:", error);
-                });
-            
-        }
+        bot.commands.get('link').execute(message, args)
     } else if(cmd === `${prefix}fact`) {
-        if(!args[0]) {
-            await getRandomFact(message);
-        } else if(args[0] === `id`) {
-            if(!args[1]) {
-                message.reply('Please add fact id!');
-            } else {
-                await getRandomFactWithId(args[1], message);
-            }
-        }
+        bot.commands.get('fact').execute(message, args)
     } else if (cmd === `${prefix}restart`) {
         await restartRequest(message);
     } else if (cmd === `${prefix}ping`) {
@@ -341,102 +316,6 @@ async function restartRequest(message: { author: { id: any; }; reply: (arg0: str
 
 }
 
-async function getRandomFactWithId(id: any, message: any) {
-    
-    const db = admin.firestore();
-    const ref = db.collection("facts").doc(id);
-    const doc = await ref.get();
-    if(!doc.exists) {
-        message.reply('Cannot find that fact!');
-    } else {
-        sendRandomFact(doc.id, message);
-    }
-}
-
-async function getRandomFact(message: any) {
-    const db = admin.firestore();
-
-    var quotes = db.collection("facts");
-
-    var key2 = quotes.doc().id;
-        
-    quotes.where(admin.firestore.FieldPath.documentId(), '>=', key2).limit(1).get()
-        .then((snapshot: { size: number; forEach: (arg0: (doc: any) => void) => void; }) => {
-            if(snapshot.size > 0) {
-                snapshot.forEach((doc: { id: any; data: () => any; }) => {
-                    sendRandomFact(doc.id, message);
-                });
-            } else {
-                quotes.where(admin.firestore.FieldPath.documentId(), '<', key2).limit(1).get()
-                    .then((snapshot: any) => {
-                        snapshot.forEach((doc: { id: any; data: () => any; }) => {
-                            sendRandomFact(doc.id, message);
-                        });
-                    })
-                    .catch((err: any) => {
-                        message.reply(`Error geting fact: **${err}**`);
-                        console.log('Error getting documents', err);
-                    });
-            }
-        })
-        .catch((err: { message: any; }) => {
-            message.reply(`Error geting fact: **${err.message}**`);
-            console.log('Error getting documents', err);
-        });
-}
-
-async function sendRandomFact(docid: any, message: { createdAt: any; channel: { send: (arg0: any) => void; }; }) {
-    const db = admin.firestore();
-    const ref = db.collection("facts").doc(docid);
-    const doc = await ref.get();
-    const userRef = db.collection('users').doc(`${doc.data()!.owner}`);
-    const userDoc = await userRef.get();
-    if(!doc.data()!.owner){
-        let upmbed = new Discord.MessageEmbed()
-            .setTitle(`Random fact`)
-            .setColor("#FFCB5C")
-            .addField("Fact", doc.data()!.fact)
-            .setFooter(`This is a template fact`)
-            .addField('\u200B', '\u200B')
-            .addField("Add your fact", `You can add your fact [here](https://facts.zal1000.com/) (to display discord info, link your discord account [here](https://dclink.zal1000.com/))`)
-            .setTimestamp(message.createdAt);
-
-        message.channel.send(upmbed);
-    
-    } else if(!userDoc.data()!.dcid) {
-
-        let upmbed = new Discord.MessageEmbed()
-            .setTitle(`Random fact by.: ${doc.data()!.author}`)
-            .setColor("#FFCB5C")
-            .addField("Fact", doc.data()!.fact)
-            .addField("Fact id", docid)
-            .addField('\u200B', '\u200B')
-            .addField("Add your fact", `You can add your fact [here](https://facts.zal1000.com/) (to display discord info, link your discord account [here](https://dclink.zal1000.com/))`)
-            .setFooter(doc.data()!.author)
-            .setTimestamp(message.createdAt);
-
-        message.channel.send(upmbed);
-        
-    } else {
-        const dcRef = db.collection('dcusers').doc(`${userDoc.data()!.dcid}`);
-        const dcDoc = await dcRef.get();
-
-        let upmbed = new Discord.MessageEmbed()
-            .setTitle(`Random fact by.: ${dcDoc.data()!.username}`)
-            .setColor("#FFCB5C")
-            .addField("Fact", doc.data()!.fact)
-            .addField("Fact id", docid)
-            .addField('\u200B', '\u200B')
-            .addField("Add your fact", `You can add your fact [here](https://facts.zal1000.com/) (to display discord info, link your discord account [here](https://dclink.zal1000.com/))`)
-            .setFooter(dcDoc.data()!.tag)
-            .setThumbnail(dcDoc.data()!.pp)
-            .setTimestamp(message.createdAt);
-
-        message.channel.send(upmbed);
-    }
-
-}
-
 async function reggeltUpdateEdit(message: { author: { id: string; }; }) {
     let db = admin.firestore();
     const botRef = db.collection("bots").doc("reggeltbot");
@@ -450,111 +329,9 @@ async function reggeltUpdateEdit(message: { author: { id: string; }; }) {
     });
 }
 
-async function getCountForUser(message: { author: { id: any; username: any; avatarURL: () => any; }; reply: (arg0: string) => void; createdAt: any; channel: { send: (arg0: any) => void; }; }) {
-    let db = admin.firestore();
-    let dcid = message.author.id;
-    const cityRef = db.collection("dcusers").doc(dcid);
-    const doc = await cityRef.get();
-    if (!doc.exists) {
-        console.log("No such document!");
-        message.reply("Error reading document!");
-    } else {
-        let upmbed = new Discord.MessageEmbed()
-            .setTitle(`${message.author.username}`)
-            .setColor("#FFCB5C")
-            .addField("Ennyiszer köszöntél be a #reggelt csatornába", `${doc.data()!.reggeltcount} [(Megnyitás a weboldalon)](https://reggeltbot.com/count?i=${dcid})`)
-            .setFooter(message.author.username)
-            .setThumbnail(message.author.avatarURL())
-            .setTimestamp(message.createdAt);
-        console.log(upmbed);
-
-        message.channel.send(upmbed);
-    }
-}
-
-async function botTypeing(channel: any) {
-    const data = JSON.stringify({});
-    console.log((await getBotToken(process.env.PROD)).token);
-      
-    const options = {
-        hostname: 'discord.com',
-        port: 443,
-        path: `/api/v8/channels/${channel}/typing`,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': data.length,
-            'Authorization': `Bot ${(await getBotToken(process.env.PROD)).token}`,
-            
-        }
-    };
-      
-    const req = https.request(options, (res: any) => {
-        console.log(`statusCode: ${res.statusCode}`);
-      
-        res.on('data', (d: string | Buffer) => {
-            process.stdout.write(d);
-        });
-    });
-      
-    req.on('error', (error: any) => {
-        console.error(error);
-    });
-      
-    req.write(data);
-    req.end();
-}
 console.log(process.env.PROD);
 const PROD = process.env.PROD;
 botlogin(PROD);
-
-async function getBotToken(PROD: string | undefined) {
-    const db = admin.firestore();
-    const botRef = db.collection("bots").doc("reggeltbot");
-    const doc = await botRef.get();
-    if(PROD === "false") {
-        return {
-            token: doc.data()!.testtoken,
-        };
-    } else if(PROD === "beta") {
-        return {
-            token: doc.data()!.betatoken,
-        };
-    } else {
-        return {
-            token: doc.data()!.token,
-        };
-    } 
-}
-
-async function accountLink(userRecord: { uid: any; }, message: { content: string; author: { id: any; }; reply: (arg0: string, arg1: undefined) => void; }) {
-    const db = admin.firestore();
-    let messageArray = message.content.split(" ");
-    //let cmd = messageArray[0];
-    let args: any = messageArray.slice(1);
-    const userRef = db.collection("users").doc(userRecord.uid);
-    const userDoc = await userRef.get();
-
-    const dcUserRef = db.collection("dcusers").doc(message.author.id);
-    // eslint-disable-next-line no-unused-vars
-    //const dcUserDoc = await dcUserRef.get();
-
-    if(userDoc.data()!.dclinked) {
-        message.reply("This account is already linked!", args[1]);
-    } else if(`${userDoc.data()!.dclink}` === args[1]) {
-        dcUserRef.update({
-            uid: message.author.id,
-        });
-        userRef.update({
-            dclink: admin.firestore.FieldValue.delete(),
-            dclinked: true,
-            dcid: message.author.id,
-        });
-        message.reply("Account linked succesfuly!", undefined);
-    } else {
-        message.reply("Error linking account", undefined);
-    }
-}
 
 async function botlogin(PROD: string | undefined) {
     const db = admin.firestore();
@@ -599,7 +376,7 @@ function msToTime(duration: number) {
     const seconds = (seconds1 < 10) ? "0" + seconds1 : seconds1;
   
     return hours + ":" + minutes + ":" + seconds;
-  }
+}
 
 
 app.listen(3000);
