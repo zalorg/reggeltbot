7811219b3dff6a2b034c4fd847ad970bf56dfd88
Zalán Horváth <pityuespetra@gmail.com>
2021-02-11 17:49:54 +0100

    Create help.ts

---
+++ src/commands/help.ts
@@ -0,0 +1,9 @@
+import * as admin from 'firebase-admin';
+import * as Discord from 'discord.js';
+module.exports = {
+    name: 'help',
+    execute(message: any) {
+        const embed = new Discord.MessageEmbed()
+
+    }
+}
