b5c257be1a0a9069dd109a9d2a22bef9051700eb
Zalán Horváth <pityuespetra@gmail.com>
2021-02-11 21:15:07 +0100

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,9 +1,13 @@
-import * as admin from 'firebase-admin';
+//import * as admin from 'firebase-admin';
 import * as Discord from 'discord.js';
 module.exports = {
     name: 'help',
     execute(message: any) {
         const embed = new Discord.MessageEmbed()
+        .setThumbnail('Help')
+        .setColor('#FFCB5C')
+        .setTimestamp(Date.now())
 
+        message.channel.send(embed);
     }
 }
