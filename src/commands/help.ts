671482cae68ccb56571022842f7d07f5dd4c96f9
Zalán Horváth <pityuespetra@gmail.com>
2021-02-12 01:26:28 +0100

    asd

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,9 +1,24 @@
-import * as admin from 'firebase-admin';
+//import * as admin from 'firebase-admin';
+import ms = require('ms');
 import * as Discord from 'discord.js';
 module.exports = {
     name: 'help',
-    execute(message: any) {
+    execute(message: any, prefix: string, bot: any) {
         const embed = new Discord.MessageEmbed()
+        .setTitle('Reggeltbot help')
+        .setColor("#FFCB2B")
+        .addField(`${prefix}count`, `Megmondja, hogy hányszor köszöntél be a #reggelt csatornába (vagy [itt](https://reggeltbot.com/count?i=${message.author.id}) is megnézheted)`)
+        .addField(`${prefix}invite`, "Bot meghívása")
+        .addField("Reggelt csatorna beállítása", "Nevezz el egy csatornát **reggelt**-nek és kész")
+        .addField("top.gg", "Ha bárkinek is kéne akkor itt van a bot [top.gg](https://top.gg/bot/749037285621628950) oldala")
+        .addField("Probléma jelentése", "Ha bármi problémát észlelnél a bot használata közben akkor [itt](https://github.com/zal1000/reggeltbot/issues) tudod jelenteni")
+        .addField('\u200B', '\u200B')
+        .addField("Bot ping", `${bot.ws.ping}ms`)
+        .addField("Uptime", `${ms(bot.uptime)}`)
+        .setFooter(message.author.username)
+        .setThumbnail(bot.user!.avatarURL()!)
+        .setTimestamp(Date.now());
 
+        message.channel.send(embed);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -190,21 +190,7 @@ bot.on("message", async (message: any) => {
     
     // help
     else if(message.content === `${prefix}help`){
-        let upmbed = new Discord.MessageEmbed()
-            .setTitle(message.author.username)
-            .setColor("#FFCB2B")
-            .addField(`${prefix}count`, `Megmondja, hogy hányszor köszöntél be a #reggelt csatornába (vagy [itt](https://reggeltbot.com/count?i=${message.author.id}) is megnézheted)`)
-            .addField(`${prefix}invite`, "Bot meghívása")
-            .addField("Reggelt csatorna beállítása", "Nevezz el egy csatornát **reggelt**-nek és kész")
-            .addField("top.gg", "Ha bárkinek is kéne akkor itt van a bot [top.gg](https://top.gg/bot/749037285621628950) oldala")
-            .addField("Probléma jelentése", "Ha bármi problémát észlelnél a bot használata közben akkor [itt](https://github.com/zal1000/reggeltbot/issues) tudod jelenteni")
-            .addField('\u200B', '\u200B')
-            .addField("Bot ping", `${bot.ws.ping}ms`)
-            .addField("Uptime", `${ms(bot.uptime)}`)
-            .setFooter(message.author.username)
-            .setThumbnail(bot.user!.avatarURL()!)
-            .setTimestamp(message.createdAt);
-        message.channel.send(upmbed);
+        bot.commands.get('help').execute(message, prefix, bot)
     }
 
     //count 
