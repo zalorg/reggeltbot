0e6558146fbd809599e679c857c0c8bf36d6d9f9
Zalán Horváth <pityuespetra@gmail.com>
2021-02-17 15:09:31 +0100

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -14,12 +14,14 @@ module.exports = {
 
         const reggeltconfig = JSON.parse(fs.readFileSync(`./lang/${currentLang.code}.json`, 'utf8')).events.reggelt;
 
+        console.log(reggeltconfig)
+
         const embed = new Discord.MessageEmbed()
         .setTitle(lang.title)
         .setColor("#FFCB2B")
-        .addField(lang.f1.replace('%!PREFIX%!', prefix), lang.f11.replace('%!DCID%!', message.author.id))
+        .addField(lang.f1.replace('%!PREFIX%!', prefix), lang.f11.replace('%!DCID%!', message.author.id).replace('%!CHANNEL%!', reggeltconfig.channel))
         .addField(lang.f2.replace('%!PREFIX%!', prefix), lang.f21)
-        .addField(lang.f3.replace('%!CHANNEL%!', reggeltconfig.channel), lang.f31.replace('%!KEYWORD%!'), reggeltconfig.keyWord)
+        .addField(lang.f3.replace('%!CHANNEL%!', reggeltconfig.channel), lang.f31.replace('%!CHANNEL%!', reggeltconfig.channel))
         .addField(lang.f4, lang.f41)
         .addField(lang.f5, lang.f51)
         .addField('\u200B', '\u200B')
