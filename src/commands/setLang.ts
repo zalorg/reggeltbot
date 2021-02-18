0bafced02755e74e35e86277006cbd56dd55e9ec
Zalán Horváth <pityuespetra@gmail.com>
2021-02-18 09:37:30 +0100

    Update setLang.ts

--- src/commands/setLang.ts
+++ src/commands/setLang.ts
@@ -4,12 +4,12 @@ import * as Discord from 'discord.js';
 
 module.exports = {
     name: 'setlang',
-    execute(message: any, args: any[]) {
-        const langs: any = JSON.parse(fs.readFileSync('./cache/langs.json', 'utf8')).langs;
+    async execute(message: any, args: any[]) {
+        const langs: any = JSON.parse(fs.readFileSync('./cache/guilds.json', 'utf8')).langs;
 
         if(!args[0]) { 
             message.reply("Please send a language code"); 
-        } else if(!args.find((e: any) => e === args[0])) {
+        } else if(!langs.find((e: any) => e === args[0])) {
             message.reply("Error: cannot find language! Here a list of all language:")
             let embed = new Discord.MessageEmbed()
             .setTitle('All avelible language')
@@ -21,15 +21,38 @@ module.exports = {
             
             return message.channel.send(embed)
         } else {
-        
-            admin.firestore().collection('bots').doc('reggeltbot').collection('langs').doc("guilds").update({
 
-            }).then(r => {
-                message.reply(`Guild language changed to: ${args[0]}!`)
-            }).catch(e => {
-                message.reply('Error updateing guild language!')
-                throw e;
-            })
+
+
+            const ref = admin.firestore().collection('bots').doc('reggeltbot').collection('config').doc(message.guild.id);
+            const doc = await ref.get();
+
+            if(message.guild.member(message.author.id).hasPermission('MANAGE_MESSAGES'))
+
+            if(doc.exists) {
+                ref.update({
+                    lang: args[0],
+                }).then(r => {
+                    message.reply(`Guild language changed to: ${args[0]}!`)
+                }).catch(e => {
+                    message.reply('Error updateing guild language!')
+                    throw e;
+                })
+            } else {
+                ref.set({
+                    lang: args[0],
+                    cd: 6,
+                    premium: false,
+                    disabled: false,
+                    testing:false,
+                }).then(r => {
+                    message.reply(`Guild language changed to: ${args[0]}!`)
+                }).catch(e => {
+                    message.reply('Error updateing guild language!')
+                    throw e;
+                })
+            }
+
         }
 
     }
