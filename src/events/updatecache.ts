03b3d82285e44ae103c9f2267e899f2734c7ef88
Zalán Horváth <pityuespetra@gmail.com>
2021-02-18 02:24:40 +0100

    Create updatecache.ts

---
+++ src/events/updatecache.ts
@@ -0,0 +1,40 @@
+import * as admin from 'firebase-admin';
+import fs = require('fs');
+module.exports = {
+    name: 'updatecache',
+    async execute() {
+        var my: Interface = {
+            langs: ["hu-HU", "en-US", "de-DE"],
+            guilds: {},
+        }
+
+        const db = admin.firestore();
+
+        const ref = db.collection('bots').doc('reggeltbot').collection('config');
+    
+        ref.onSnapshot(s => {
+            updatecache(my).then(() => {
+                fs.writeFileSync('./cache/guilds.json', JSON.stringify(my))
+            })
+        })
+
+    }
+}
+
+interface Interface {
+    guilds: { [key: string]: any };
+    langs: Array<string>;
+}
+
+async function updatecache(data: Interface) {
+    const db = admin.firestore();
+
+    const ref = db.collection('bots').doc('reggeltbot').collection('config');
+    const doc = await ref.get();
+
+    doc.forEach(item => {
+        data.guilds[item.id] = item.data();
+    })
+}
+
+//            fs.writeFileSync('./cache/guilds.json', JSON.stringify(my))
