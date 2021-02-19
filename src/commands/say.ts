2ee0b55a59755101906f02f6659486e7969b1842
Zalán Horváth <pityuespetra@gmail.com>
2021-02-19 15:10:01 +0100

    Update say.ts

--- src/commands/say.ts
+++ src/commands/say.ts
@@ -1,18 +1,23 @@
 import * as textToSpeech from '@google-cloud/text-to-speech'
 import fs = require('fs');
-//import { Message } from 'discord.js'
+import { Message } from 'discord.js'
 
 module.exports = {
     name: 'say',
-    async execute(message: any, args: Array<string>,) {
-        if(!args) return message.reply('I cant say nothing')
+    async execute(message: Message, args: Array<string>,) {
+        //if(!args) return message.reply('I cant say nothing')
         
         const client = new textToSpeech.TextToSpeechClient();
         const text = args.join(' ');
-          
+
+        const langcode = JSON.parse(fs.readFileSync('./cache/guilds.json', 'utf8'));
+        const currentLang = langcode.guilds[message.guild!.id]
+
+        message.reply(`${currentLang.lang}`)
+
             const [response] = await client.synthesizeSpeech({
                 input: {text: text},
-                voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+                voice: {languageCode: `${currentLang.lang}`, ssmlGender: 'NEUTRAL'},
                 audioConfig: {audioEncoding: 'MP3'},
               });
 
@@ -21,10 +26,13 @@ module.exports = {
             console.log('Audio content written to file: output.mp3');
 
             
-            var voiceChannel = message.member.voice.channel;
-            voiceChannel.join().then((connection: any) => {
-                message.member.voice.channel.join().then((VoiceConnection: any) => {
-                    VoiceConnection.play(`./cache/${message.author.id}.mp3`).on("finish", () => VoiceConnection.disconnect());
+            var voiceChannel = message.member!.voice.channel;
+            voiceChannel!.join().then((connection: any) => {
+                message.member!.voice.channel!.join().then((VoiceConnection: any) => {
+                    VoiceConnection.play(`./cache/${message.author.id}.mp3`).on("finish", () => {
+                        VoiceConnection.disconnect();
+                        message.reply('Disconnected')
+                    });
                 }).catch((e: any) => console.log(e))
 
              }).catch((err: any) => console.log(err));
