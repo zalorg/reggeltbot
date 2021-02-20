cdf37584c948145b4de4f560804a963f5da0c161
Zalán Horváth <pityuespetra@gmail.com>
2021-02-20 01:59:03 +0100

    Update messageUpdate.ts

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -1,25 +1,27 @@
 import * as admin from 'firebase-admin';
 import fs = require('fs');
+import { Client, Message, PartialMessage } from 'discord.js'
+
 module.exports =  {
     name: 'msgUpdate',
-    execute(bot: any) {
+    execute(bot: Client) {
 
 
-        bot.on("messageUpdate", async (_: any, newMsg: any) => {
-            if(newMsg.author.bot) return;
+        bot.on("messageUpdate", async (_, newMsg) => {
+            if(newMsg.author!.bot) return;
 
-            const langcode = JSON.parse(fs.readFileSync('./cache/langs.json', 'utf8'));
+        const langcode = JSON.parse(fs.readFileSync('./cache/guilds.json', 'utf8'));
 
-            const currentLang = langcode.guilds.find((element: any) => element.id === newMsg.guild.id)
+        const currentLang = langcode.guilds[newMsg.guild!.id]
             
             const lang = JSON.parse(fs.readFileSync(`./lang/${currentLang.code}.json`, 'utf8')).events.reggelt;
         
-            if(newMsg.channel.name === lang.events.reggelt.channel){
-                if(!newMsg.content.toLowerCase().includes(lang.events.reggelt.keyWord)) {
+            if(newMsg.channel.type === "text" && newMsg.channel.name === lang.events.reggelt.channel){
+                if(!newMsg.content!.toLowerCase().includes(lang.events.reggelt.keyWord)) {
                     await reggeltUpdateEdit(newMsg);
                     if(newMsg.deletable){
                         newMsg.delete();
-                        newMsg.author.send(lang.editSend.replace('%!GUILD%!', `${newMsg.guild.name}`));
+                        newMsg.author!.send(lang.editSend.replace('%!GUILD%!', `${newMsg.guild!.name}`));
                     }
                 }
             }
@@ -27,7 +29,7 @@ module.exports =  {
     }
 }
 
-async function reggeltUpdateEdit(message: { author: { id: string; }; }) {
+async function reggeltUpdateEdit(message: Message | PartialMessage) {
     let db = admin.firestore();
     const botRef = db.collection("bots").doc("reggeltbot");
     const botDoc = await botRef.get();
@@ -35,7 +37,7 @@ async function reggeltUpdateEdit(message: { author: { id: string; }; }) {
     await db.collection("bots").doc("reggeltbot-count-all").update({
         reggeltcount: admin.firestore.FieldValue.increment(decreaseCount)
     });
-    await db.collection("dcusers").doc(message.author.id).update({
+    await db.collection("dcusers").doc(message.author!.id).update({
         reggeltcount: admin.firestore.FieldValue.increment(decreaseCount)
     });
 }
