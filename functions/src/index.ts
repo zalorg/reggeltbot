4e9fb020bab9d5696c89f008bbb0de899db47467
Zalán Horváth <pityuespetra@gmail.com>
2021-02-21 20:23:40 +0100

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,9 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as Discord from 'discord.js';
+admin.initializeApp()
+
+const client = new Discord.Client()
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,7 +12,20 @@ import * as functions from 'firebase-functions';
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-export const senddcmessage = functions.firestore.document('bots/reggeltbot/sendmessages/{docId}').onCreate((snapshot, context) => {
+export const senddcmessage = functions.firestore.document('bots/reggeltbot/sendmessages/{docId}').onCreate(async (snapshot, context) => {
+
+    const channel = client.channels.cache.find(channel => channel.id === snapshot.data().id)
+
+    console.log(channel);
+
+
+
+    const db = admin.firestore()
+    const botRef = db.collection("bots").doc("reggeltbot");
+    const doc = await botRef.get();
+
+    client.login(doc.data()!.token)
+
     console.log(snapshot.data());
     console.log(context);
 });
