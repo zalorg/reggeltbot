b7a216dd1ee463b8829042ebe3a72831562d9aff
Zalán Horváth <pityuespetra@gmail.com>
2021-02-21 16:35:28 +0100

    Beta test ready

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,4 +7,7 @@ import * as functions from 'firebase-functions';
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-export const senddcmessage = functions
+export const senddcmessage = functions.firestore.document('bots/reggeltbot/sendmessages/{docId}').onCreate((snapshot, context) => {
+    console.log(snapshot.data());
+    console.log(context);
+});
