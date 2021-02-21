173fbfb087b2c58e8850929bc4a3ddec0a1dd165
Zalán Horváth <pityuespetra@gmail.com>
2021-02-21 13:33:57 +0100

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,8 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,4 +11,9 @@ import * as functions from 'firebase-functions';
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-export const senddcmessage = functions
+export const senddcmessage = functions.firestore.document('bots/reggeltbot/sendmessage/{docId}').onCreate((change, context) => {
+    admin.firestore().collection('bots').doc('reggeltbot').collection('messages').add({
+        context: context,
+        change: change,
+    });
+});
