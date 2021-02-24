38b5cb6b0defca59a74b8c58791b89d9fc2bac29
Zalán Horváth <pityuespetra@gmail.com>
2021-02-24 20:58:54 +0100

    Update ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,11 +2,15 @@ import * as admin from 'firebase-admin';
 import fs = require('fs');
 import DBL = require('dblapi.js');
 import { Client } from 'discord.js'
+import * as axios from 'axios';
 
 module.exports = {
     name: 'ready',
     execute(bot: Client) {
         bot.on("ready", async () => {
+
+            sendCommands()
+
             const dblToken = (await admin.firestore().collection('bots').doc('reggeltbot').get()).data()?.dblToken
             const dbl = new DBL(dblToken, bot);
             dbl.on('posted', () => {
@@ -40,4 +44,50 @@ module.exports = {
         
         });
     }
+}
+
+function sendCommands() {
+
+    send({
+        "name": "count",
+        "description": "Ennyiszer köszöntél be a #reggelt csatornába",
+        "options": [
+            {
+                "name": "user",
+                "description": "The user to get",
+                "type": 6,
+                "required": false
+            }
+        ]
+    })
+    
+}
+
+async function send(data: any) {
+    axios.default.post('', {
+        data: data,
+        headers: {
+            'Authorization': `Bot ${(await getBotToken())}`,
+        }
+    })
+}
+
+async function getBotToken() {
+    const PROD = process.env.PROD;
+    const db = admin.firestore();
+    const botRef = db.collection("bots").doc("reggeltbot");
+    const doc = await botRef.get();
+    if(PROD === "false") {
+        return {
+            token: doc.data()!.testtoken,
+        };
+    } else if(PROD === "beta") {
+        return {
+            token: doc.data()!.betatoken,
+        };
+    } else {
+        return {
+            token: doc.data()!.token,
+        };
+    } 
 }
