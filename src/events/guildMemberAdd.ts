9b7add9b04ec9fc97e82894b7b2bd17d161e7bfa
Zalán Horváth <pityuespetra@gmail.com>
2021-02-25 20:32:28 +0100

    Update guildMemberAdd.ts

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -10,7 +10,40 @@ module.exports = {
 
             if(doc.exists) {
                 ref.set({
-                    
+                    pp: member.user.avatarURL(),
+                    tag: member.user.tag,
+                    username: member.user.username,
+                    discriminator: member.user.discriminator,
+                }).catch(err => {throw err});
+
+                ref.collection('guilds').doc(member.guild.id).set({
+                    allpermissions: member.permissions.toArray(),
+                    color: member.displayColor,
+                    colorHEX: member.displayHexColor,
+                    icon: member.guild.iconURL(),
+                    nick: member.nickname,
+                    owner: member.guild.ownerID,
+                    permissions: {
+                        ADMINISTRATOR: member.hasPermission("ADMINISTRATOR"),
+                        MANAGE_CHANNELS: member.hasPermission("MANAGE_CHANNELS"),
+                        MANAGE_GUILD: member.hasPermission("MANAGE_GUILD"),
+                        MANAGE_MESSAGES: member.hasPermission("MANAGE_MESSAGES"),                
+                    },
+                })
+            } else {
+                ref.collection('guilds').doc(member.guild.id).set({
+                    allpermissions: member.permissions.toArray(),
+                    color: member.displayColor,
+                    colorHEX: member.displayHexColor,
+                    icon: member.guild.iconURL(),
+                    nick: member.nickname,
+                    owner: member.guild.ownerID,
+                    permissions: {
+                        ADMINISTRATOR: member.hasPermission("ADMINISTRATOR"),
+                        MANAGE_CHANNELS: member.hasPermission("MANAGE_CHANNELS"),
+                        MANAGE_GUILD: member.hasPermission("MANAGE_GUILD"),
+                        MANAGE_MESSAGES: member.hasPermission("MANAGE_MESSAGES"),                
+                    },
                 })
             }
         });
