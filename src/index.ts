0e06d750e55a776af7714bacd21f2a514455b083
Zalán Horváth <pityuespetra@gmail.com>
2021-02-26 12:17:30 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const bot = new Discord.Client();
 import * as admin from 'firebase-admin';
 import express = require('express');
 import { Guildconfig, Regggeltconfig } from './types'
+import { Client, GuildMember } from "discord.js";
 
 const app = express();
 
@@ -228,6 +229,8 @@ bot.on("message", async message => {
                 }).then(d => console.log(`${member.user.username} added`)).catch(e => console.log(e));
             }
         })
+    } else if(message.guild && message.guild.id === "541446521313296385" && message.author.id === "423925286350880779" && cmd === `${prefix}waikupdate`) {
+        waikupdate(bot)
     }
 });
 
@@ -349,6 +352,92 @@ function msToTime(duration: number) {
     return hours + ":" + minutes + ":" + seconds;
 }
 
+async function waikupdate(bot: Client) {
+    const db = admin.firestore();
+
+    const waik = await bot.guilds.fetch('541446521313296385');
+
+    const metrics = db.doc(`bots/reggeltbot/config/${waik.id}`);
+
+    metrics.update({
+        m3: 0,
+        m6: 0,
+        y1: 0,
+        y2: 0,
+    })
+    const logchannel = await bot.channels.fetch('763040615080263700', false, true);
+    if(logchannel.isText()) {
+        logchannel.send("Waik members sync started")
+    }
+    waik.members.cache.forEach(async member => {
+
+
+        //const joinref = await admin.firestore().doc(`dcusers/${member.id}/guilds/${waik.id}`).get();
+        const join = member.joinedTimestamp!;
+        const m3 = 3 * 2592000000;
+        const m6 = 6 * 2592000000;
+        const y1 = 12 * 2592000000;
+        const y2 = 24 * 2592000000;
+        const now = Date.now();
+
+        //console.log(join)
+
+
+        if(join + y2 < now) {
+            waik.member(member.id)?.roles.remove("814303109966200864").then(async member => {
+                if(logchannel.isText()) {
+                    logchannel.send("Waik members sync started")
+                }
+            }).catch
+
+            waik.member(member.id)?.roles.add('814303501512343622').then(member => sendlog(member, undefined, "y2")).catch(e => sendlog(member, e.message, "y2"))
+            metrics.update({
+                y2: admin.firestore.FieldValue.increment(1),
+            })
+        } else if(join + y1 < now) {
+            waik.member(member.id)?.roles.add('814303109966200864').then(member => sendlog(member, undefined, "y1")).catch(e => sendlog(member, e.message, "y1"))
+            metrics.update({
+                y1: admin.firestore.FieldValue.increment(1),
+            })
+        } else if(join + m6 < now) {
+            waik.member(member.id)?.roles.add('814302832031301683').then(member => sendlog(member, undefined, "m2")).catch(e => sendlog(member, e.message, "m6"))
+            metrics.update({
+                m6: admin.firestore.FieldValue.increment(1),
+            })
+        } else if(join + m3 < now) {
+            //waik.member(member.id)?.roles.add('814302832031301683').catch(e => console.error(e.message))
+            metrics.update({
+                m3: admin.firestore.FieldValue.increment(1),
+            })
+        }
+
+    })
+
+    async function sendlog(member: GuildMember, err: string | undefined, role: string) {
+        const logchannel = await bot.channels.fetch('763040615080263700', false, true);
+        if(logchannel.isText()) {
+            if(err) {
+                if(role === "y2") {
+                    logchannel.send(`<@${member.id}> failed to added to **y2** Error: **${err}**`)
+                } else if(role === "y1") {
+                    logchannel.send(`<@${member.id}> failed to added to **y1** Error: **${err}**`)
+                } else if(role === "m6") {
+                    logchannel.send(`<@${member.id}> failed to added to **m6** Error: **${err}**`)
+                }
+
+                // Err end
+            } else {
+                if(role === "y2") {
+                    logchannel.send(`<@${member.id}> added to **y2**`)
+                } else if(role === "y1") {
+                    logchannel.send(`<@${member.id}> added to **y1**`)
+                } else if(role === "m6") {
+                    logchannel.send(`<@${member.id}> added to **m6**`)
+                }
+            }
+        }
+    }
+}
 
 app.listen(3000);
 
