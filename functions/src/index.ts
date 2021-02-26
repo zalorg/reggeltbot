dfae1ddcf7336dc69cb566d55dc841f61ce4a3ae
Zalán Horváth <pityuespetra@gmail.com>
2021-02-26 10:08:31 +0100

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -104,82 +104,106 @@ export const waik_roles_update = functions.pubsub.topic('waik-roles-update').onP
         })
 
         bot.login(doc.data()!.token);
+
+        return;
     }, 60000);
 
 },)
 
 export const updateAllGuild = functions.pubsub.topic('updateAllGuild').onPublish(async message => {
-    const bot = new Discord.Client();
+    functions.logger.info('function started')
+    setTimeout(async () => {
+        const bot = new Discord.Client();
 
-    const botRef = db.collection("bots").doc("reggeltbot");
-    const doc = await botRef.get();
-
-    bot.on('ready', async () =>{
-
-        bot.guilds.cache.forEach(guild => {
-            guild.members.cache.forEach(async member => {
-                const ref = admin.firestore().doc(`/dcusers/${member.id}/guilds/${guild.id}`);
-                const doc = await ref.get();
-                const gme = member
-
-                const userref = admin.firestore().doc(`dcusers/${member.id}`);
-                //const userdoc = await userref.get();
-
-                if(!member.user.bot) {
-                    userref.set({
-                        pp: member.user.avatarURL({dynamic: true}),
-                        tag: member.user.tag,
-                        username: member.user.username,
-                        discriminator: member.user.discriminator,
-                    }, {merge: true}).catch(e => console.error(e));
-                }
-
-                if(member.user.bot){ 
-                    console.log('bot ignored')
-                } else if(doc.exists) {
-                    ref.set({
-                        joinedTimestamp: member.joinedTimestamp,
-                        joinedAt: member.joinedAt,
-                        name: guild.name,
-                        owner: guild.ownerID,
-                        icon: guild.iconURL(),
-                        permissions: {
-                            ADMINISTRATOR: gme.hasPermission("ADMINISTRATOR"),
-                            MANAGE_CHANNELS: gme.hasPermission("MANAGE_CHANNELS"),
-                            MANAGE_GUILD: gme.hasPermission("MANAGE_GUILD"),
-                            MANAGE_MESSAGES: gme.hasPermission("MANAGE_MESSAGES"),                
-                        },
-                        allpermissions: gme.permissions.toArray(),
-                        color: member.displayColor,
-                        colorHEX: member.displayHexColor,
-                        nick: member.nickname,
-                    }, {merge: true}).then(d => console.log(`${member.user.username} updated`)).catch(e => console.log(e));
-                } else {
-                    ref.set({
-                        joinedTimestamp: member.joinedTimestamp,
-                        joinedAt: member.joinedAt,
-                        name: guild.name,
-                        owner: guild.ownerID,
-                        icon: guild.iconURL(),
-                        permissions: {
-                            ADMINISTRATOR: gme.hasPermission("ADMINISTRATOR"),
-                            MANAGE_CHANNELS: gme.hasPermission("MANAGE_CHANNELS"),
-                            MANAGE_GUILD: gme.hasPermission("MANAGE_GUILD"),
-                            MANAGE_MESSAGES: gme.hasPermission("MANAGE_MESSAGES"),                
-                        },
-                        allpermissions: gme.permissions.toArray(),
-                        color: member.displayColor,
-                        colorHEX: member.displayHexColor,
-                        nick: member.nickname,
-                    }).then(d => console.log(`${member.user.username} added`)).catch(e => console.log(e));
-                }
-            })
-        })
-    })
-
+        const botRef = db.collection("bots").doc("reggeltbot");
+        const doc = await botRef.get();
     
+        bot.on('ready', async () => {
+    
+            const logsguild = await bot.guilds.fetch('738169002085449748');
+            const logschannel = logsguild.channels.cache.find(c => c.name === "gcloud-logs")
+            if(logschannel && logschannel.isText()) {
+                logschannel.send('updateAllGuild started')
+            }
+    
+            bot.guilds.cache.forEach(guild => {
+                guild.members.cache.forEach(async member => {
+                    const ref = admin.firestore().doc(`/dcusers/${member.id}/guilds/${guild.id}`);
+                    const doc = await ref.get();
+                    const gme = member
+    
+                    const userref = admin.firestore().doc(`dcusers/${member.id}`);
+                    //const userdoc = await userref.get();
+    
+                    if(!member.user.bot) {
+                        userref.set({
+                            pp: member.user.avatarURL({dynamic: true}),
+                            tag: member.user.tag,
+                            username: member.user.username,
+                            discriminator: member.user.discriminator,
+                        }, {merge: true}).catch(e => console.error(e));
+                    }
+    
+                    if(member.user.bot){ 
+                        console.log('bot ignored')
+                    } else if(doc.exists) {
+                        ref.set({
+                            joinedTimestamp: member.joinedTimestamp,
+                            joinedAt: member.joinedAt,
+                            name: guild.name,
+                            owner: guild.ownerID,
+                            icon: guild.iconURL(),
+                            permissions: {
+                                ADMINISTRATOR: gme.hasPermission("ADMINISTRATOR"),
+                                MANAGE_CHANNELS: gme.hasPermission("MANAGE_CHANNELS"),
+                                MANAGE_GUILD: gme.hasPermission("MANAGE_GUILD"),
+                                MANAGE_MESSAGES: gme.hasPermission("MANAGE_MESSAGES"),                
+                            },
+                            allpermissions: gme.permissions.toArray(),
+                            color: member.displayColor,
+                            colorHEX: member.displayHexColor,
+                            nick: member.nickname,
+                        }, {merge: true}).then(d => console.log(`${member.user.username} updated`)).catch(e => errhandler(e));
+                    } else {
+                        ref.set({
+                            joinedTimestamp: member.joinedTimestamp,
+                            joinedAt: member.joinedAt,
+                            name: guild.name,
+                            owner: guild.ownerID,
+                            icon: guild.iconURL(),
+                            permissions: {
+                                ADMINISTRATOR: gme.hasPermission("ADMINISTRATOR"),
+                                MANAGE_CHANNELS: gme.hasPermission("MANAGE_CHANNELS"),
+                                MANAGE_GUILD: gme.hasPermission("MANAGE_GUILD"),
+                                MANAGE_MESSAGES: gme.hasPermission("MANAGE_MESSAGES"),                
+                            },
+                            allpermissions: gme.permissions.toArray(),
+                            color: member.displayColor,
+                            colorHEX: member.displayHexColor,
+                            nick: member.nickname,
+                        }).then(d => console.log(`${member.user.username} added`)).catch(e => errhandler(e));
+                    }
+                })
+                if(logschannel && logschannel.isText()) {
+                    logschannel.send(`Function **updateAllGuild** finished`)
+                }
 
-    bot.login(doc.data()!.token);
+            })
+    
+            async function errhandler(e: any) {
+                if(logschannel && logschannel.isText()) {
+                    logschannel.send(`Error in **updateAllGuild** function: ${e.message}`)
+                }
+            }
+        })
+    
+    
+        bot.login(doc.data()!.token); 
+
+        return;
+    }, 60000);
+
+
 })
 
 export const updateAllGuild2 = functions.https.onRequest(async (req, res) => {
