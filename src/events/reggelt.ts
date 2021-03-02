f4b24982feb6aa28b3ae0245567feb3639ba37b2
Zalán Horváth <pityuespetra@gmail.com>
2021-03-02 03:53:11 +0100

    Removed 2 lines

--- src/events/reggelt.ts
+++ src/events/reggelt.ts
@@ -7,8 +7,6 @@ module.exports = {
     async execute(message: Message) {
         const db = admin.firestore();
 
-        
-
         const guildconfig: Guildconfig = JSON.parse(fs.readFileSync('./cache/guilds.json', 'utf8')).guilds[message.guild!.id];
 
         const guildlang = guildconfig.lang || "en-US"
