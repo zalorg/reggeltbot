68492206fa7a6ab527823902a4c2c35ab9b49acb
Zalán Horváth <pityuespetra@gmail.com>
2021-03-12 23:11:36 +0100

    Update manager.ts

--- src/manager.ts
+++ src/manager.ts
@@ -28,12 +28,36 @@ async function botlogin(PROD: string | undefined) {
 async function start() {
     const manager = await botlogin(process.env.PROD);
 
-manager.on('shardCreate', async shard => {
-    console.log(`Shard created (${shard.id})`)
-});
+    manager.on('shardCreate', async shard => {
+        console.log(`Shard created (${shard.id})`)
+    });
 
-manager.spawn('auto');
+    manager.spawn('auto');
+
+    app.post('/shard', async (req, res) => {
+        const id = Number(req.query.id);
+        if(id === NaN) return res.status(400).send({message: "NaN"});
+        manager.createShard(id);
+        return res.status(200).send({message: `shard created (${id})`})
+    })
+
+    app.patch('/shard', async (req, res) => {
+        return manager.respawnAll().then(f => {
+            res.status(200).send({message: 'shards respwned'})
+        }).catch(e => {
+            res.status(500).send({message: 'error respawning shards', error: e.message})
+            console.error(e)
+        })
+    })
+
+    app.delete('/shard', async (req, res) => {
+        manager
+    })
 
 }
+
+app.get('/ping', async (req, res) => {
+    res.status(200).send({message: 'OK'});
+})
 
 app.listen(3000);
