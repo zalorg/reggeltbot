c62d5f071e5caf6fe8ad0818b2106c609ae22c8b
Zalán Horváth <pityuespetra@gmail.com>
2021-03-16 22:20:06 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -92,6 +92,7 @@ app.patch('/waikroleupdate', async (req, res) => {
     res.sendStatus(200);
 })
 
+app.listen(process.env.PORT || 3000)
 
 bot.on('error', async (err: any) => {
     console.log(err);
