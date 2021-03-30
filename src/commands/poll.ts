639735c0e0c723e1fbef988c3cb9d53e742228c6
Zalán Horváth <pityuespetra@gmail.com>
2021-03-30 10:40:31 +0200

    Update poll.ts

--- src/commands/poll.ts
+++ src/commands/poll.ts
@@ -7,7 +7,10 @@ const db = admin.firestore()
 module.exports = {
     name: 'pollCmd',
     async execute(message: Discord.Message, args: string[], bot: Discord.Client) {
-
+        if(message.member?.permissions.has('MANAGE_MESSAGES')) {
+            message.reply('nope! :)')
+            return;
+        }
         var clr = () => {
             console.log(1)
         }
